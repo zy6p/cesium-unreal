66b1b4edbd5085e45d4d48fe1befcfa226533de1
knowmore <goooog@88.com>
2021-06-06 00:00:46 +0800

    fix: linux clang path

--- extern/build-linux.sh
+++ extern/build-linux.sh
@@ -8,8 +8,8 @@ cmake -B build -S . \
   -DCMAKE_SYSTEM_NAME=Linux \
   -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
   -DCMAKE_SYSROOT="${UNREAL_ENGINE_COMPILER_DIR}" \
-  -DCMAKE_C_COMPILER="${UNREAL_ENGINE_COMPILER_DIR}" \
-  -DCMAKE_CXX_COMPILER="${UNREAL_ENGINE_COMPILER_DIR}" \
+  -DCMAKE_C_COMPILER="${UNREAL_ENGINE_COMPILER_DIR}"/bin/clang \
+  -DCMAKE_CXX_COMPILER="${UNREAL_ENGINE_COMPILER_DIR}"/bin/clang++ \
   -DCMAKE_CXX_FLAGS="-nostdinc++ -I${UNREAL_ENGINE_LIBCXX_DIR}/include -I${UNREAL_ENGINE_LIBCXX_DIR}/include/c++/v1 -I${UNREAL_ENGINE_LIBCXX_DIR}/include/c++/v1" \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DCMAKE_BUILD_TYPE=Release \
@@ -20,4 +20,4 @@ cmake -B build -S . \
   -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ONLY
 
 cmake --build build --target install -j12
-${UNREAL_ENGINE_DIR}/Engine/Build/BatchFiles/RunUAT.sh BuildPlugin -Plugin="${PWD}/CesiumForUnreal.uplugin" -Package="${PWD}/../../packages/CesiumForUnreal" -CreateSubFolder -TargetPlatforms=Linux
+${UNREAL_ENGINE_DIR}/Engine/Build/BatchFiles/RunUAT.sh BuildPlugin -Plugin="${PWD}/../CesiumForUnreal.uplugin" -Package="${PWD}/../../packages/CesiumForUnreal" -CreateSubFolder -TargetPlatforms=Linux
